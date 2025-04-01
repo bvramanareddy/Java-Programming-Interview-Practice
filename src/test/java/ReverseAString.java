47b74829186bac429458e772e44157564343a7cb
Ramana <v_boda@yahoo.com>
2025-04-01 13:52:16 -0400

    Adding Files

---
+++ src/test/java/ReverseAString.java
@@ -0,0 +1,12 @@
+public class ReverseAString {
+
+    public static String reverseTheGivenString(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "ramana";
+        System.out.println(reverseTheGivenString(s));
+    }
+}
